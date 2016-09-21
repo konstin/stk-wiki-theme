81cf0fa5cc2a97654ef2ad1f22d42ba2531a9b41
konstin <konstin@mailbox.org>
2016-09-21 23:28:29 +0200

    Rewrite the readme and add a full LocalSettings.php file

---
+++ LocalSettings.template.php
@@ -0,0 +1,159 @@
+<?php
+# LocalSetting.php for the skin of https://supertuxkart.net
+
+# Protect against web entry
+if ( !defined( 'MEDIAWIKI' ) ) {
+    exit;
+}
+
+###
+# Actual options
+###
+
+$wgSitename = "SuperTuxKart";
+$wgServer = "https://supertuxkart.net";
+
+# Database settings
+$wgDBtype = "mysql";
+$wgDBserver = "localhost";
+$wgDBname = "stk";
+$wgDBuser = "stk";
+$wgDBpassword = "*************";
+
+# Shared memory settings
+$wgMainCacheType = CACHE_ANYTHING;
+$wgMemCachedServers = array();
+
+# Piwik - comment out to deactivate
+require_once "$IP/extensions/Piwik/Piwik.php";
+$wgPiwikURL = "piwik.supertuxkart.net";
+$wgPiwikIDSite = "1";
+
+###
+# Other Settings
+###
+
+# The URL structure
+$wgScriptPath = "";
+$wgArticlePath = "/$1";
+$wgScriptExtension = ".php";
+$wgUsePathInfo = true;
+$wgResourceBasePath = $wgScriptPath;
+
+# The skin
+require_once "$IP/skins/SuperTuxKart/SuperTuxKart.php";
+$wgDefaultSkin = "SuperTuxKart";
+$wgStylePath   = "$wgScriptPath/skins";
+$wgLogo        = "$wgStylePath/SuperTuxKart/images/logo.png";
+$wgFavicon     = "$wgStylePath/SuperTuxKart/images/favicon.png";
+
+# Extensions
+require_once "$IP/extensions/YouTube/YouTube.php";
+require_once "$IP/extensions/CSS/CSS.php";
+require_once "$IP/extensions/NativeSvgHandler/NativeSvgHandler.php";
+require_once "$IP/extensions/IframePage/IframePage.php";
+wfLoadExtension('ParserFunctions');
+
+# Add custom namespace
+# https://www.mediawiki.org/wiki/Manual:Using_custom_namespaces
+define("NS_LEGACY", 3000); // This MUST be even.
+$wgExtraNamespaces[NS_LEGACY] = "Legacy";
+define("NS_LEGACY_TALK", 3001); // This MUST be the following odd integer.
+$wgExtraNamespaces[NS_LEGACY_TALK] = "Legacy_talk"; // Note underscores in the namespace name.
+
+
+# Prevent arbitrary account creation
+$wgGroupPermissions['*']['createaccount'] = false;
+$wgGroupPermissions['*']['edit']          = false;
+$wgGroupPermissions['*']['read']          = true;
+$wgGroupPermissions['*']['createtalk']    = false;
+
+# Necessary for e.g. the download page
+$wgRawHtml = true;
+
+# Image options
+$wgEnableUploads = true;
+$wgUseImageMagick = true;
+$wgImageMagickConvertCommand = "/usr/bin/convert";
+$wgFileExtensions[] = 'svg';
+$wgMaxImageArea = 100000000;
+
+# Allow embedding itch.io
+$wgIframePageSrc =  ['itchio' => 'https://itch.io/embed/83502'];
+
+# wtf bots?
+# https://www.mediawiki.org/wiki/Manual:Bot_passwords
+$wgEnableBotPasswords = false;
+
+###
+# Secrets
+###
+
+$wgSecretKey = "REPLACE WITH REAL SECRET KEY";
+
+# Site upgrade key. Must be set to a string (default provided) to turn on the
+# web installer while LocalSettings.php is in place
+$wgUpgradeKey = "REPLACE WITH REAL UPGRADE KEY";
+
+###
+# Mediawiki boilerplate
+###
+
+## For attaching licensing metadata to pages, and displaying an
+## appropriate copyright notice / icon. GNU Free Documentation
+## License and Creative Commons licenses are supported so far.
+$wgRightsPage = ""; # Set to the title of a wiki page that describes your license/copyright
+$wgRightsUrl  = "";
+$wgRightsText = "";
+$wgRightsIcon = "";
+
+# Path to the GNU diff3 utility. Used for conflict resolution.
+$wgDiff3 = "/usr/bin/diff3";
+
+# MySQL table options to use during installation or update
+$wgDBTableOptions = "ENGINE=InnoDB, DEFAULT CHARSET=binary";
+
+# Experimental charset support for MySQL 5.0.
+$wgDBmysql5 = false;
+
+# InstantCommons allows wiki to use images from https://commons.wikimedia.org
+$wgUseInstantCommons = false;
+
+## If you use ImageMagick (or any other shell command) on a
+## Linux server, this will need to be set to the name of an
+## available UTF-8 locale
+$wgShellLocale = "en_US.utf8";
+
+## Set $wgCacheDirectory to a writable directory on the web server
+## to make your wiki go slightly faster. The directory should not
+## be publically accessible from the web.
+#$wgCacheDirectory = "$IP/cache";
+
+# Site language code, should be one of the list in ./languages/Names.php
+$wgLanguageCode = "en";
+
+## UPO means: this is also a user preference option
+$wgEnableEmail         = true;
+$wgEnableUserEmail     = true; # UPO
+$wgEnotifUserTalk      = false; # UPO
+$wgEnotifWatchlist     = false; # UPO
+$wgEmailAuthentication = true;
+
+$wgEmergencyContact = "mediawiki@" . $wgSitename;
+$wgPasswordSender   = "mediawiki@" . $wgSitename;
+
+# MySQL specific settings
+$wgDBprefix = "";
+
+
+###
+# For Debugging
+# DISABLE AFTER FINDING BUG
+###
+/*
+error_reporting(-1);
+ini_set('display_errors', 1);
+$wgShowSQLErrors = true;
+$wgDebugDumpSql = true;
+$wgShowDBErrorBacktrace = true;
+*/
--- README.md
+++ README.md
@@ -1,43 +1,25 @@
 # STK-wiki-theme
 
-A modern and responsive wiki skin for SuperTuxKart.
+A modern and responsive wiki skin for the website of SuperTuxKart.
 
 ## Installation
-The theme is based on mediawiki, so first of all you'll need to download and setup MediaWiki.
+A webserver with PHP and mysql and a mail server are required.
 
-Once mediawiki is ready, you need to copy this skin to the `skins` folder. Now you should have a new folder called `skins/stk-wiki-theme`. Rename that folder to `SuperTuxKart`.
+Download MediaWiki and go to the mediawiki directory. Clone this skin into a
+folder called `skins/SuperTuxKart`. Copy `skins/SuperTuxKart/LocalSettings.template.php`
+to `LocalSettings.php`. Replace the values of `$wgSecretKey` and `$wgUpgradeKey`
+with real secret keys.
 
-If you want to properly run the SuperTuxKart wiki, you also need to install three mediawiki extensions: [Youtube](https://www.mediawiki.org/wiki/Extension:YouTube), [CSS](https://www.mediawiki.org/wiki/Extension:CSS) and [Purge](https://www.mediawiki.org/wiki/Extension:Purge).
+Create a mysql database with a corresponding user and put their values in the
+database section of `LocalSettings.php`.
 
-To configure everything as needed for the SuperTuxKart wiki, add the following at the end of `LocalSettings.php`:
+Download the following extension and unpack them in the `extensions` folder.
+ * [Youtube](https://www.mediawiki.org/wiki/Extension:YouTube)
+ * [CSS](https://www.mediawiki.org/wiki/Extension:CSS)
+ * [NativeSvgHandler](https://www.mediawiki.org/wiki/Extension:NativeSvgHandler).
+ * [IframePage](https://www.mediawiki.org/wiki/Extension:IframePage).
 
-```
-# Enable the skin
-require_once "$IP/skins/SuperTuxKart/SuperTuxKart.php";
-$wgDefaultSkin = "SuperTuxKart";
-$wgStylePath   = "$wgScriptPath/skins";
-$wgLogo        = "$wgStylePath/SuperTuxKart/images/logo.png";
-$wgFavicon     = "$wgStylePath/SuperTuxKart/images/favicon.png";
-
-# Enable the extensions (optional)
-require_once "$IP/extensions/YouTube/YouTube.php";
-require_once "$IP/extensions/CSS/CSS.php";
-require_once "$IP/extensions/Purge/Purge.php";
-
-# Prevent account creation
-$wgGroupPermissions['*']['createaccount'] = false;
-$wgGroupPermissions['*']['edit'] = false;
-$wgGroupPermissions['*']['read'] = true;
-$wgGroupPermissions['*']['createtalk'] = false;
-
-# Necessary for e.g. the download page
-$wgRawHtml = true; # Not the best solution, but still better than using javascript
-
-# Enable file uploads and also enable svg
-$wgEnableUploads = true;
-$wgFileExtensions[] = 'svg';
-wfLoadExtension( 'ParserFunctions' );
-```
+Now you can configure your webserver to serve the wiki.
 
 ## Coding Convetions
  * All stylesheets are written in less, stored in the `css/` folder and included in `supertuxkart.less`.
@@ -46,4 +28,3 @@ wfLoadExtension( 'ParserFunctions' );
  * Avoid javascript wherever possible
  * Avoid loading resources from external server due to privacy and availability issues
  * All custom fonts are saved in the `fonts` folder in different formats and included using `@font-face`
-
