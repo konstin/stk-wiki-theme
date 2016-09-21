6d089fa9b3fd8406d4a1d19a1f2c161063a2e542
konstin <konstin@mailbox.org>
2016-09-21 23:28:17 +0200

    remove the old skin

--- stk-oldskin/stk_skin/scripts/stk.init.ie6.js
+++
@@ -1,3 +0,0 @@
-document.stopObserving('dom:loaded', STK.WikiControls.init);
-//document.stopObserving('dom:loaded', STK.ManageSections.init.bind(this, 'Downloads', 'downloads_container', '3'));
-document.stopObserving('dom:loaded', STK.SearchTidy.init);
--- stk-oldskin/stk_skin/scripts/stk.init.js
+++
@@ -1,4 +0,0 @@
-document.observe('dom:loaded', STK.WikiControls.init);
-document.observe('dom:loaded', STK.ManageSections.init.bind(this, 'Downloads', 'downloads_container', '3'));
-document.observe('dom:loaded', STK.SearchTidy.init);
-
